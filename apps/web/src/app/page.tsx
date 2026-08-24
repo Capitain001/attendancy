097473faa82965ea08c12cdaa93ff13b2ecaf30b
Capitain001 <capitainstuart@gmail.com>
2026-08-24 08:24:29 +0000

    feat: initialize multi-tenant schema with base identity, organization, and RBAC models

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -6,24 +6,24 @@ import Link from "next/link";
 export default async function HomePage() {
   const user = await getUserInfo();
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex flex-col min-h-screen items-center justify-center gap-4">
 
       <h1 className="text-2xl font-semibold">attendancy</h1>
 
 
       {/* NB:cette section sert temporairement a rendre accessible le lien de l ettablissement dispo dans le header pr ceux qui test la maquette */}
       {/* cette page est volontairement minimaliste */}
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center gap-4 ">
         {user ?
-          <span>
+          <div>
             <OrgLink user={user} />
-          </span>
-          : <Link href={"/login"}>
+          </div>
+          : <Link href={"/login"} className="flex">
             connecter vous
           </Link>
         }
 
-        <span>
+        <span className="">
           <a href={"https://github.com/Capitain001/attendancy"} target="_blank" className="p-2 border rounded- bg-muted/80 ">
             <Github size={24} />
           </a>
